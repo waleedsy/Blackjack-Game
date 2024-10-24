03c7be2f047badfa32de03edcdee6783d17642e8
waleedsy <waleedsani2013@gmail.com>
2024-10-25 00:11:24 +0100

    Calculate cards score

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import randorm
+import random
 
 def deal_card():
     """Returns a randorm card from the deck"""
     cards = [11, 2, 3, 4,5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = randorm.choice(cards)
+    card = random.choice(cards)
     return card
 
 user_cards = []
@@ -13,4 +13,14 @@ for _ in range(2):
     new_card = deal_card()
     user_cards.append(new_card)
     
-    computer_cards.append(deal_card)
+    computer_cards.append(deal_card) 
+    
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+      
+    return sum(cards)
