81fd7fffc15931d2562f029f53d11d1d7fc6fac6
waleedsy <waleedsani2013@gmail.com>
2024-10-27 00:50:38 +0100

    final touches

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ def deal_card():
     card = random.choice(cards)
     return card
 
+    
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw!"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack!"
+    elif user_score == 0:
+        return "Win with a Blackjack!!!"
+    elif user_score > 21:
+        return "You went over, you lose!"
+    elif computer_score > 21:
+        return "opponent went over, you win!!!"
+    elif user_score > computer_score:
+        return "You win!!!"
+    else:
+        return "You lose!"
 
 user_cards = []
 computer_cards = []
@@ -41,10 +57,12 @@ while not is_game_over:
     else:
         user_should_deal = input(":Type 'y' to get another card, type 'n' to pass: ") 
         if user_should_deal == "y":
-            user_cards.append(deal_card())
+             user_cards.append(deal_card())
         else:
             is_game_over = True   
             
 while computer_score != 0 and computer_score < 17:
     computer_score.append(deal_card())
-    computer_score = calculate_score(computer_cards)     
+    computer_score = calculate_score(computer_cards)   
+      
+print(compare(user_score, computer_score))
